e91755f9b0c758ab587c504a5521aa021d390378
Administrator <627535440@qq.com>
2014-06-05 23:53:34 +0800

    tank

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
-  res.write('asdasdad')
-  res.end();
-}).listen(process.env.PORT || 1337, null);
+var http = require( 'http' );
+var fs = require( 'fs' );
+
+http.createServer( function( req, res ) {
+	res.writeHead( 200, {'Content-Type': 'text/html;charset=utf-8'} );
+	var url = req.url;
+	if(url == '/') {
+		fs.readFile( 'index.html', 'utf8', function( err, data ) {
+			res.write( data );
+			res.end();
+		} );
+
+	}else{
+		fs.readFile(url, 'utf8', function( err, data ) {
+			res.write( data );
+			res.end();
+		} );
+	}
+
+
+} ).listen( process.env.PORT || 1337, null );
