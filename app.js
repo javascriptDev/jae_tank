799924fb68a8209f67d3520ca4f8b6fbbe503508
Administrator <627535440@qq.com>
2014-06-06 00:11:20 +0800

    tank

--- app.js
+++ app.js
@@ -2,19 +2,26 @@ var http = require( 'http' );
 var fs = require( 'fs' );
 
 http.createServer( function( req, res ) {
-	res.writeHead( 200, {'Content-Type': 'text/html;charset=utf-8'} );
+
 	var url = req.url;
+
 	if(url == '/') {
+		res.writeHead( 200, {'Content-Type': 'text/html;charset=utf-8'} );
 		fs.readFile( 'index.html', 'utf8', function( err, data ) {
 			res.write( data );
 			res.end();
 		} );
 
-	}else{
-		fs.readFile(url, 'utf8', function( err, data ) {
-			res.write( data );
-			res.end();
-		} );
+	} else {
+		var a = url.split( '.' )[1] == 'js' ? 'application/x-javascript' : 'text/css';
+		var u = url.replace( '/', '' );
+		res.writeHead( 200, {'Content-Type': +a + ';charset=utf-8'} );
+		if(u != 'favicon.ico') {
+			fs.readFile( url.replace( '/', '' ), 'utf8', function( err, data ) {
+				res.write( data );
+				res.end();
+			} );
+		}
 	}
 
 
